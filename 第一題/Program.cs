f1a73cbfe92f801c4d69c7290adb21d14bcdb8b0
黃祈瀚 <eric20645@gmail.com>
2023-02-21 11:13:13 +0800

    第一題完成

--- 第一題/Program.cs
+++ 第一題/Program.cs
@@ -10,6 +10,21 @@ namespace 第一題
     {
         static void Main(string[] args)
         {
+            string outputs = "";
+            Console.WriteLine("請輸入一串以逗號分隔的字串，並將其反向顯示");
+            string input = Console.ReadLine();
+            Console.WriteLine($"輸入:{input}");
+            List<string> output = input.Split(',').ToList();
+            output.Reverse();
+
+            for(int i = 0; i < output.Count; i++)
+            {
+                outputs+= output[i]+',';
+            }
+            outputs = outputs.Substring(0, outputs.Length - 1);
+            Console.WriteLine($"輸出:{outputs}");
+            
+
         }
     }
 }
